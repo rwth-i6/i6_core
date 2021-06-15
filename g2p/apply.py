e6e5bcd11a4574a8731df071d70414ec49f741ef
Nick Rossenbach <nick.rossenbach@rwth-aachen.de>
2021-06-15 13:36:59 +0200

    Refactor g2p jobs (#53)

--- g2p/apply.py
+++ g2p/apply.py
@@ -4,19 +4,34 @@ import subprocess as sp
 
 from sisyphus import *
 
+from recipe.i6_core.util import uopen
+
 Path = setup_path(__package__)
 
 
 class ApplyG2PModelJob(Job):
+    """
+    Apply a trained G2P on a word list file
+    """
+
     def __init__(
         self,
         g2p_model,
-        word_list,
+        word_list_file,
         variants_mass=1.0,
         variants_number=1,
         g2p_path=None,
         g2p_python=None,
     ):
+        """
+        :param Path g2p_model:
+        :param Path word_list_file: text file with a word each line
+        :param float variants_mass:
+        :param int variants_number:
+        :param Path|str|None g2p_path:
+        :param Path|str|None g2p_python:
+        """
+
         if g2p_path is None:
             g2p_path = tk.gs.G2P_PATH
         if g2p_python is None:
@@ -27,15 +42,15 @@ class ApplyG2PModelJob(Job):
         self.g2p_python = g2p_python
         self.variants_mass = variants_mass
         self.variants_number = variants_number
-        self.word_list = word_list
+        self.word_list = word_list_file
 
-        self.g2p_lexicon = self.output_path("g2p.lexicon")
+        self.out_g2p_lexicon = self.output_path("g2p.lexicon")
 
     def tasks(self):
         yield Task("run", mini_task=True)
 
     def run(self):
-        with open(self.g2p_lexicon.get_path(), "wt") as out:
+        with uopen(self.out_g2p_lexicon, "wt") as out:
             sp.check_call(
                 [
                     self.g2p_python,
@@ -47,9 +62,9 @@ class ApplyG2PModelJob(Job):
                     "--variants-number",
                     str(self.variants_number),
                     "-m",
-                    tk.uncached_path(self.g2p_model),
+                    self.g2p_model.get_path(),
                     "-a",
-                    tk.uncached_path(self.word_list),
+                    self.word_list.get_path(),
                 ],
                 stdout=out,
             )
--- g2p/train.py
+++ g2p/train.py
@@ -5,13 +5,21 @@ import subprocess as sp
 
 from sisyphus import *
 
+from recipe.i6_core.util import uopen
+
 Path = setup_path(__package__)
 
 
 class TrainG2PModelJob(Job):
+    """
+    Train a G2P model using Sequitur
+
+    see https://github.com/sequitur-g2p/sequitur-g2p
+    """
+
     def __init__(
         self,
-        train_lexicon,
+        g2p_lexicon,
         num_ramp_ups=4,
         min_iter=1,
         max_iter=60,
@@ -21,6 +29,20 @@ class TrainG2PModelJob(Job):
         g2p_path=None,
         g2p_python=None,
     ):
+        """
+
+        :param Path g2p_lexicon: g2p_lexicon for training, use BlissLexiconToG2PLexiconJob to generate a g2p_lexicon
+            from a bliss lexicon
+        :param int num_ramp_ups: number of global ramp-ups (n-gram-iness)
+        :param int min_iter: minimum iterations per ramp-up
+        :param int max_iter: maximum iteration sper ramp-up
+        :param str devel: passed as -d argument, percent of train lexicon held out as validation set
+        :param str size_constrains: passed as -s argument,
+            multigrams must have l1 ... l2 left-symbols and r1 ... r2 right-symbols
+        :param list[str] extra_args: extra cmd arguments that are passed to the g2p process
+        :param Path|str|None g2p_path:
+        :param Path|str|None g2p_python:
+        """
         if extra_args is None:
             extra_args = []
         if g2p_path is None:
@@ -28,7 +50,7 @@ class TrainG2PModelJob(Job):
         if g2p_python is None:
             g2p_python = tk.gs.G2P_PYTHON
 
-        self.train_lexicon = train_lexicon
+        self.g2p_lexicon = g2p_lexicon
         self.num_ramp_ups = num_ramp_ups
         self.min_iter = min_iter
         self.max_iter = max_iter
@@ -38,14 +60,14 @@ class TrainG2PModelJob(Job):
         self.g2p_path = g2p_path
         self.g2p_python = g2p_python
 
-        self.g2p_models = [
+        self.out_g2p_models = [
             self.output_path("model-%d" % idx) for idx in range(self.num_ramp_ups + 1)
         ]
-        self.error_rates = [
+        self.out_error_rates = [
             self.output_var("err-%d" % idx) for idx in range(self.num_ramp_ups + 1)
         ]
-        self.best_model = self.output_path("model-best")
-        self.best_error_rate = self.output_var("err-best")
+        self.out_best_model = self.output_path("model-best")
+        self.out_best_error_rate = self.output_var("err-best")
 
         self.rqmt = {
             "time": max(0.5, (self.max_iter / 20.0) * (self.num_ramp_ups + 1)),
@@ -58,7 +80,7 @@ class TrainG2PModelJob(Job):
 
     def run(self):
         for idx in range(self.num_ramp_ups + 1):
-            if os.path.exists(self.g2p_models[idx].get_path()):
+            if os.path.exists(self.out_g2p_models[idx].get_path()):
                 continue
 
             args = [
@@ -78,29 +100,29 @@ class TrainG2PModelJob(Job):
                 "tmp-model",
                 "-S",
                 "-t",
-                tk.uncached_path(self.train_lexicon),
+                self.g2p_lexicon.get_path(),
             ]
             if idx > 0:
-                args += ["-r", "-m", self.g2p_models[idx - 1].get_path()]
+                args += ["-r", "-m", self.out_g2p_models[idx - 1].get_path()]
             args += self.extra_args
 
             if os.path.exists("tmp-model"):
                 os.unlink("tmp-model")
 
-            with open("stdout.%d" % idx, "w") as out:
+            with uopen("stdout.%d" % idx, "w") as out:
                 sp.check_call(args, stdout=out)
 
-            with open("stdout.%d" % idx, "rt") as log:
+            with uopen("stdout.%d" % idx, "rt") as log:
                 for line in log:
                     if "total symbol errors" in line:
                         error_rate = float(line.split("(")[1].split("%")[0])
-                        self.error_rates[idx].set(error_rate)
+                        self.out_error_rates[idx].set(error_rate)
 
-            os.rename("tmp-model", self.g2p_models[idx].get_path())
+            os.rename("tmp-model", self.out_g2p_models[idx].get_path())
 
         best = min(
-            ((idx, err_var.get()) for idx, err_var in enumerate(self.error_rates)),
+            ((idx, err_var.get()) for idx, err_var in enumerate(self.out_error_rates)),
             key=lambda t: t[1],
         )
-        os.symlink("model-%d" % best[0], self.best_model.get_path())
-        self.best_error_rate.set(best[1])
+        os.symlink("model-%d" % best[0], self.out_best_model.get_path())
+        self.out_best_error_rate.set(best[1])
