9fd64cabf709bdceee0466b4e05551d2d6870e3d
Nick Rossenbach <nick.rossenbach@rwth-aachen.de>
2021-07-06 12:37:46 +0200

    Add consistency check to Returnn config (#64)

--- returnn/search.py
+++ returnn/search.py
@@ -172,6 +172,7 @@ class ReturnnSearchJob(Job):
         res = copy.deepcopy(returnn_config)
         res.config = config
         res.post_config = post_config
+        res.check_consistency()
 
         return res
 
