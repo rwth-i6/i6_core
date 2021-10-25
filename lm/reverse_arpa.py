c833967d9af36e78bdedc5174f10accfb04084e1
Mattia Di Gangi <mdigangi@apptek.com>
2021-10-25 10:08:17 +0200

    Fix ReverseArpaLmJob to work with the new LM object

--- lm/reverse_arpa.py
+++ lm/reverse_arpa.py
@@ -1,6 +1,5 @@
 __all__ = ["ReverseARPALmJob"]
 
-import gzip
 import sys
 import typing
 
@@ -29,82 +28,84 @@ class ReverseARPALmJob(Job):
         # Adapted for python 3
         lm = Lm.from_arpa(self.lm_path)
 
-        outfile = util.uopen(self.reverse_lm_path.get_path(), "wt", encoding="utf-8")
+        with util.uopen(self.out_reverse_lm.get_path(), "wt", encoding="utf-8") as outfile:
 
-        # compute new reversed ARPA model
-        outfile.write("\\data\\\n")
-        for n in range(1, len(lm.ngram_counts) + 1):  # unigrams, bigrams, trigrams
-            outfile.write("ngram %d=%d\n" % (n, len(lm.ngram_counts[n - 1])))
-        offset = 0.0
-        all_ngrams = []
-        for n in range(1, len(lm.ngram_counts) + 1):  # unigrams, bigrams, trigrams
-            curr_ngrams = sorted(lm.get_ngrams(n))
-            all_ngrams.append(curr_ngrams)
-            for ngram in curr_ngrams:
-                words = ngram.split(" ")
-                self.add_missing_backoffs(words, all_ngrams)
-        for n in range(1, len(lm.ngram_counts) + 1):
-            outfile.write("\\%d-grams:\n" % n)
-            for ngram in all_ngrams[n - 1]:
-                prob = lm.ngrams[n - 1][ngram]
-                # reverse word order
-                words = ngram.split()
-                rstr = " ".join(reversed(words))
-                # swap <s> and </s>
-                rev_ngram = (
-                    rstr.replace("<s>", "<temp>")
-                    .replace("</s>", "<s>")
-                    .replace("<temp>", "</s>")
-                )
+            # compute new reversed ARPA model
+            all_ngrams = []
+            for n in range(1, len(lm.ngram_counts) + 1):  # unigrams, bigrams, trigrams
+                curr_ngrams = {name: prob for name, prob in lm.get_ngrams(n)}
+                all_ngrams.append(curr_ngrams)
+                for ngram in curr_ngrams:
+                    words = ngram.split(" ")
+                    ReverseARPALmJob.add_missing_backoffs(words, all_ngrams)
 
-                revprob = prob[0]
-                if prob[1] != float(
-                    "inf"
-                ):  # only backoff weights from not newly created ngrams
-                    revprob = revprob + prob[1]
-                # sum all missing terms in decreasing ngram order
-                for x in range(n - 1, 0, -1):
-                    l_ngram = " ".join(words[:x])  # shortened ngram
-                    if l_ngram not in lm.ngrams[x - 1]:
-                        sys.stderr.write("%s: not found %s\n" % (rev_ngram, l_ngram))
-                    p_l = lm.ngrams[x - 1][l_ngram][0]
-                    revprob = revprob + p_l
+            outfile.write("\\data\\\n")
+            for n in range(1, len(lm.ngram_counts) + 1):  # unigrams, bigrams, trigrams
+                outfile.write("ngram %d=%d\n" % (n, len(all_ngrams[n-1])))
+            offset = 0.0
+            for n in range(1, len(lm.ngram_counts) + 1):
+                outfile.write("\\%d-grams:\n" % n)
+                for ngram in sorted(all_ngrams[n - 1]):
+                    prob = all_ngrams[n - 1][ngram]
+                    # reverse word order
+                    words = ngram.split()
+                    rstr = " ".join(reversed(words))
+                    # swap <s> and </s>
+                    rev_ngram = (
+                        rstr.replace("<s>", "<temp>")
+                        .replace("</s>", "<s>")
+                        .replace("<temp>", "</s>")
+                    )
 
-                    r_ngram = " ".join(
-                        words[1 : 1 + x]
-                    )  # shortened ngram with offset one
-                    if r_ngram not in lm.ngrams[x - 1]:
-                        sys.stderr.write("%s: not found %s\n" % (rev_ngram, r_ngram))
-                    p_r = lm.ngrams[x - 1][r_ngram][0]
-                    revprob = revprob - p_r
-
-                if n != len(lm.ngram_counts):  # not highest order
-                    back = 0.0
-                    if (
-                        rev_ngram[:3] == "<s>"
-                    ):  # special handling since arpa2fst ignores <s> weight
-                        if n == 1:
-                            offset = revprob  # remember <s> weight
-                            revprob = lm.sentprob  # apply <s> weight from forward model
-                            back = offset
-                        elif n == 2:
-                            revprob = (
-                                revprob + offset
-                            )  # add <s> weight to bigrams starting with <s>
+                    revprob = prob[0]
                     if prob[1] != float(
                         "inf"
                     ):  # only backoff weights from not newly created ngrams
-                        outfile.write("%g %s %g\n" % (revprob, rev_ngram, back))
-                    else:
-                        outfile.write("%g %s -100000.0\n" % (revprob, rev_ngram))
-                else:  # highest order - no backoff weights
-                    if (n == 2) and (rev_ngram[:3] == "<s>"):
-                        revprob = revprob + offset
-                    outfile.write("%g %s\n" % (revprob, rev_ngram))
-        outfile.write("\\end\\\n")
+                        revprob = revprob + prob[1]
+                    # sum all missing terms in decreasing ngram order
+                    for x in range(n - 1, 0, -1):
+                        l_ngram = " ".join(words[:x])  # shortened ngram
+                        if l_ngram not in all_ngrams[x - 1]:
+                            sys.stderr.write("%s: not found %s\n" % (rev_ngram, l_ngram))
+                        p_l = all_ngrams[x - 1][l_ngram][0]
+                        revprob = revprob + p_l
 
+                        r_ngram = " ".join(
+                            words[1 : 1 + x]
+                        )  # shortened ngram with offset one
+                        if r_ngram not in all_ngrams[x - 1]:
+                            sys.stderr.write("%s: not found %s\n" % (rev_ngram, r_ngram))
+                        p_r = all_ngrams[x - 1][r_ngram][0]
+                        revprob = revprob - p_r
+
+                    if n != len(lm.ngram_counts):  # not highest order
+                        back = 0.0
+                        if (
+                            rev_ngram[:3] == "<s>"
+                        ):  # special handling since arpa2fst ignores <s> weight
+                            if n == 1:
+                                offset = revprob  # remember <s> weight
+                                revprob = lm.sentprob  # apply <s> weight from forward model
+                                back = offset
+                            elif n == 2:
+                                revprob = (
+                                    revprob + offset
+                                )  # add <s> weight to bigrams starting with <s>
+                        if prob[1] != float(
+                            "inf"
+                        ):  # only backoff weights from not newly created ngrams
+                            outfile.write("%g %s %g\n" % (revprob, rev_ngram, back))
+                        else:
+                            outfile.write("%g %s -100000.0\n" % (revprob, rev_ngram))
+                    else:  # highest order - no backoff weights
+                        if (n == 2) and (rev_ngram[:3] == "<s>"):
+                            revprob = revprob + offset
+                        outfile.write("%g %s\n" % (revprob, rev_ngram))
+            outfile.write("\\end\\\n")
+
+    @staticmethod
     def add_missing_backoffs(
-        self, words, ngrams: typing.List[typing.Dict[str, typing.Tuple]]
+        words, ngrams: typing.List[typing.Dict[str, typing.Tuple]]
     ):
         n = len(ngrams)
         inf = float("inf")
