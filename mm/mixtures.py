5e1628a197c0fc20aaa7794329b3ce27d916fe57
Timur Schuemann <timur.schuemann@rwth-aachen.de>
2022-01-16 16:55:40 +0100

    added correct bit-wise modes to the call

--- mm/mixtures.py
+++ mm/mixtures.py
@@ -117,7 +117,9 @@ class MergeMixturesJob(rasr.RasrCommand, Job):
             util.partition_into_tree(self.mixtures_to_combine, self.combine_per_step),
         )
         shutil.move(mixtures, self.out_mixtures.get_path())
-        os.chmod(self.out_mixtures.get_path, stat.S_IROTH)
+
+        update_mode = stat.S_IRUSR | stat.S_IWUSR | stat.S_IROTH | stat.S_IRGRP
+        os.chmod(self.out_mixtures.get_path(), update_mode)
 
     def cleanup_before_run(self, cmd, retry, *args):
         log = args[2][12:]
