b603c4318e7e44e2aa26a818f77d677fde9f288a
michelwi <michelwi@users.noreply.github.com>
2022-03-28 16:54:41 +0200

    fix local file caching for ReturnnDumpHDFJob (#234)

--- returnn/hdf.py
+++ returnn/hdf.py
@@ -1,12 +1,13 @@
 __all__ = ["ReturnnDumpHDFJob", "ReturnnRasrDumpHDFJob"]
 
 import os
+import shutil
 import subprocess as sp
 import tempfile
 
 from .rasr_training import ReturnnRasrTrainingJob
 import i6_core.rasr as rasr
-from i6_core.util import relink, instanciate_delayed
+from i6_core.util import instanciate_delayed
 
 from sisyphus import *
 
@@ -79,7 +80,7 @@ class ReturnnDumpHDFJob(Job):
             data = data.get_path()
 
         (fd, tmp_hdf_file) = tempfile.mkstemp(prefix=gs.TMP_PREFIX, suffix=".hdf")
-        fd.close()
+        os.close(fd)
 
         args = [
             tk.uncached_path(self.returnn_python_exe),
@@ -95,7 +96,7 @@ class ReturnnDumpHDFJob(Job):
             args.append(f"--epoch {self.epoch}")
 
         sp.check_call(args)
-        relink(tmp_hdf_file, self.out_hdf.get_path())
+        shutil.move(tmp_hdf_file, self.out_hdf.get_path())
 
     @classmethod
     def hash(cls, parsed_args):
