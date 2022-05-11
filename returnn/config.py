b42a484677fdc44aec45d5d18c1a8a8acc2709f8
Albert Zeyer <zeyer@cs.rwth-aachen.de>
2022-05-11 21:46:44 +0200

    ReturnnConfig, docstring update, fix types
    Fixed order.
    
    Fixed too long line.
    
    Fixed type of hashes.

--- returnn/config.py
+++ returnn/config.py
@@ -88,14 +88,15 @@ class ReturnnConfig:
 
         :param dict config: dictionary of the RETURNN config variables that are hashed
         :param dict post_config: dictionary of the RETURNN config variables that are not hashed
+        :param None|dict[int, dict[str, Any]] staged_network_dict: dictionary of network dictionaries,
+            indexed by the desired starting epoch of the network stage.
+            By enabling this, an additional "networks" folder will be created next to the config location
         :param None|str|Callable|Class|tuple|list|dict python_prolog: str or structure containing str/callables/classes
             that should be pasted as code at the beginning of the config file
-        :param None|dict[int, dict[str, Any]] staged_network_dict: dictionary of network dictionaries, indexed by the desired starting epoch of the network stage
-            By enabling this, an additional "networks" folder will be created next to the config location
-        :param str|None python_prolog_hash: sets a specific hash for the python_prolog
+        :param None|Any python_prolog_hash: sets a specific hash for the python_prolog
         :param None|str|Callable|Class|tuple|list|dict python_epilog: str or structure containing
             str/callables/classes that should be pasted as code at the end of the config file
-        :param str|None python_epilog_hash: sets a specific hash for the python_epilog
+        :param None|Any python_epilog_hash: sets a specific hash for the python_epilog
         :param bool hash_full_python_code: By default, function bodies are not hashed. If set to True, the full content
             of python pro-/epilog is parsed and hashed.
         :param bool sort_config: If set to True, the dictionary part of the config is sorted by key
