4a14c14f2b62c226eaf914f0d618b131efc0a321
Christoph M. Lüscher <chris.luscher@gmx.de>
2022-07-06 11:19:39 +0200

    more
    Co-authored-by: michelwi <michelwi@users.noreply.github.com>

--- corpus/convert.py
+++ corpus/convert.py
@@ -1,5 +1,5 @@
 __all__ = [
-    "CorpusReplaceOrthFromReferenceCorpusJob",
+    "CorpusReplaceOrthFromReferenceCorpus",
     "CorpusReplaceOrthFromTxtJob",
     "CorpusToStmJob",
     "CorpusToTextDictJob",
