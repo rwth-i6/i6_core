d77b169ab1e4728adfd9a29654a20043b061edab
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2022-07-26 17:42:59 +0200

    first draft of get length hdfs

--- returnn/dataset.py
+++ returnn/dataset.py
@@ -5,7 +5,7 @@ from sisyphus import *
 import os
 import shutil
 import subprocess
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 import numpy
 
@@ -95,31 +95,60 @@ class ExtractDatasetMeanStddevJob(Job):
 
 def get_returnn_length_hdfs(
     dataset_dict: Dict[str, Any],
-    data_dim: int,
+    extern_data: Dict[str, Any],
     dataset_keys: List,
     returnn_exe: tk.Path,
     returnn_root: tk.Path,
-):
+    job_alias: Optional[str] = None,
+) -> Dict[str, tk.Path]:
+    """
+    Uses returnn to extract the length of sequences in the datasets after feature extraction, returns a separate hdf
+    file per dataset key
+    :param dataset_dict: RETURNN config dict for dataset
+    :param extern_data: extern data dict matching the dataset
+    :param dataset_keys: keys of the datastreams
+    :param returnn_exe:
+    :param returnn_root:
+    :param job_alias: full alias of the forward job
+    :return:
+    """
     post_config = {"use_tensorflow": True}
     config = {
         "eval": dataset_dict,
         "network": {
-            "output": {"class": "length", "axis": "T", "from": f"data:{dataset_keys[0]}"},
-            f"length_{idx}": {"class": "length", "axis": "T", "from": f"data:{key}"} for idx, key in enumerate(dataset_keys)
-            f"dump_{idx}": {"class": "hdf_dump", "filename": f"{key}.hdf", "from": f"length_data_{idx}"} for idx, key in enumerate(dataset_keys)
+            "output": {
+                "class": "length",
+                "axis": "T",
+                "from": f"data:{dataset_keys[0]}",
+            },
         },
-        "extern_data": {data_key: {"available_for_inference": True, "dim": data_dim, "shape": (None, data_dim)}}
+        "extern_data": extern_data,
     }
-    for idx, key in enumerate(dataset_keys):
-        config["network"][f"length_{idx}"] = {"class": "length", "axis": "T", "from": f"data:{key}"}
-        config["network"][f"dump_{idx}"] = {"class": "hdf_dump", "filename": f"{key}.hdf", "from": f"length_data_{idx}"}
+    for idx, key in enumerate(dataset_keys[1:]):
+        config["network"][f"length_{idx}"] = {
+            "class": "length",
+            "axis": "T",
+            "from": f"data:{key}",
+        }
+        config["network"][f"dump_{idx}"] = {
+            "class": "hdf_dump",
+            "filename": f"{key}.hdf",
+            "from": f"length_data_{idx}",
+        }
 
+    hdf_outputs = [k + ".hdf" for k in dataset_keys]
     config = ReturnnConfig(config=config, post_config=post_config)
     forward_job = ReturnnForwardJob(
         model_checkpoint=None,
         returnn_config=config,
         returnn_python_exe=returnn_exe,
         returnn_root=returnn_root,
-        hdf_outputs=dataset_keys,
+        hdf_outputs=hdf_outputs,
     )
-    return forward_job.out_hdf_files
+    if job_alias is not None:
+        forward_job.add_alias(job_alias)
+    # remove .hdf extension and map default output to first dataset key
+    hdf_dict = {k[:-4]: v for k, v in forward_job.out_hdf_files}
+    hdf_dict[dataset_keys[0]] = hdf_dict["output"]
+    del hdf_dict["output"]
+    return hdf_dict
