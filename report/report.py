11eaacb0fb58dc7a3b52206dc8f4c26dbbac2b5f
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2022-09-22 23:58:04 +0200

    import and black

--- report/report.py
+++ report/report.py
@@ -8,45 +8,44 @@ import gzip
 
 
 class ReportFinishedOutput(Job):
+    def __init__(
+        self,
+        name: str,
+        output: tk.Path,
+    ):
+        self.name = name
+        self.output = output
+        self.out = self.output_path("report.gz")
 
-  def __init__(self,
-    name: str,
-    output: tk.Path,
-  ):
-    self.name = name
-    self.output = output
-    self.out = self.output_path('report.gz')
+        if hasattr(global_settings, "GLOBAL_SETTINGS_FILE"):
+            with open(global_settings.GLOBAL_SETTINGS_FILE) as f:
+                self.settings = f.read()
+        else:
+            self.settings = ""
 
-    if hasattr(global_settings, "GLOBAL_SETTINGS_FILE"):
-      with open(global_settings.GLOBAL_SETTINGS_FILE) as f:
-        self.settings = f.read()
-    else:
-      self.settings = ''
+        self.sis_command_line = sys.argv
+        self.cwd = os.path.abspath(".")
+        self.report_format = pprint.pformat
+        self.mail_address = getattr(global_settings, "MAIL_ADDRESS", None)
 
-    self.sis_command_line = sys.argv
-    self.cwd = os.path.abspath('.')
-    self.report_format = pprint.pformat
-    self.mail_address = getattr(global_settings, 'MAIL_ADDRESS', None)
+    def run(self):
+        user = getpass.getuser()
+        report = {
+            "output": self.output.get_path(),
+            "user": user,
+            "name": self.name,
+            "sis_command_line": self.sis_command_line,
+            "cwd": self.cwd,
+        }
 
+        report = eval(repr(report))
+        pprint.pprint(report)
 
-  def run(self):
-    user = getpass.getuser()
-    report = {
-        "output": self.output.get_path(),
-        "user": user,
-        "name": self.name,
-        "sis_command_line": self.sis_command_line,
-        "cwd": self.cwd,
-    }
+        with gzip.open(str(self.out), "w") as f:
+            f.write(self.report_format(report).encode() + b"\n")
 
-    report = eval(repr(report))
-    pprint.pprint(report)
+        if self.mail_address:
+            self.sh("zcat {out} | mail -s 'Report finished: {name} ' {mail_address}")
 
-    with gzip.open(str(self.out), 'w') as f:
-      f.write(self.report_format(report).encode() + b'\n')
-
-    if self.mail_address:
-      self.sh('zcat {out} | mail -s \'Report finished: {name} \' {mail_address}')
-
-  def tasks(self):
-    yield Task('run', mini_task=True)
+    def tasks(self):
+        yield Task("run", mini_task=True)
