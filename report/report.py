9bf63c6c8fe735541fc948bc549e1d57959d2b01
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2022-09-27 18:23:13 +0200

    more updates

--- report/report.py
+++ report/report.py
@@ -3,127 +3,101 @@ from sisyphus import *
 import sys
 import getpass
 import gzip
-from datetime import datetime
-from typing import Dict, Union, Callable, Optional, Iterable
+from typing import Dict, Union, Callable, Optional
+import pprint
 
 _Report_Type = Dict[str, Union[tk.AbstractPath, str]]
 
 
+class GenerateReportStringJob(Job):
+    """
+    Job to generate and output a report string
+    """
+
+    def __init__(
+        self,
+        report_values: _Report_Type,
+        report_template: Optional[Callable[[_Report_Type], str]] = None,
+        compress: bool = True,
+    ):
+        """
+
+        :param report_values:
+        :param report_template:
+        :param compress:
+        """
+        self.report_values = report_values
+        self.report_template = report_template
+        self.compress = compress
+
+        self.out_report = self.output_path("report.gz" if self.compress else "report")
+
+    def tasks(self):
+        yield Task("run", mini_task=True)
+
+    def run(self):
+
+        if self.report_template:
+            report = self.report_template(self.report_values)
+        elif callable(self.report_values):
+            report = str(self.report_values())
+        else:
+            report = pprint.pformat(self.report_values, width=140)
+
+        if self.compress:
+            with gzip.open(self.out_report.get_path(), "wt") as f:
+                f.write(report)
+        else:
+            with open(self.out_report.get_path(), "wt") as f:
+                f.write(report)
+
+
 class MailJob(Job):
     """
-    Job that sends a mail
+    Job that sends a mail upon completion of an output
     """
 
     def __init__(
         self,
-        subject: str,
-        content: str,
+        result: tk.AbstractPath,
+        subject: Optional[str] = None,
         mail_address: str = getpass.getuser(),
-        wait_on: Optional[Union[Iterable[tk.AbstractPath], tk.AbstractPath]] = None,
-        compress: bool = True,
+        send_contents: bool = False,
     ):
         """
 
+        :param result: graph output that triggers sending the mail
         :param subject: Subject of the mail
-        :param mail_address: Mail address of the user
-        :param content:
-        :param wait_on:
+        :param mail_address: Mail address of recipient (default: user)
+        :param send_contents: send the contents of result in body of the mail
         """
+        self.result = result
         self.subject = subject
         self.mail_address = mail_address
-        self.content = content
-        self.compress = compress
+        self.send_contents = send_contents
 
-        if wait_on is not None:
-            if isinstance(wait_on, tk.AbstractPath):
-                self.add_input(wait_on)
-            elif isinstance(wait_on, Iterable):
-                for wait in wait_on:
-                    self.add_input(wait)
-            else:
-                assert False, "Wrong wait_on input!"
-
-        self.out_content = self.output_path("content")
+        self.out_status = self.output_var("out_status")
 
     def tasks(self):
         yield Task("run", mini_task=True)
 
     def run(self):
 
-        if self.compress:
-            with gzip.open(self.out_content.get_path(), "w") as f:
-                f.write(self.content.encode() + b"\n")
+        if self.subject is None:
+            subject = f"Output {str(self.result)} is finished"
         else:
-            with open(self.out_content.get_path(), "w") as f:
-                f.write(self.content)
+            subject = self.subject
 
-        self.sh("zcat -f {out_content} | mail -s '{subject}' {mail_address}")
-
-
-class ReportResultsJob(Job):
-    """
-    Job to report results either via mail and/or just condensed in a file
-    """
-
-    def __init__(
-        self,
-        name: str,
-        report: _Report_Type,
-        report_format: Callable[[_Report_Type, any], str],
-        compress: bool = True,
-        **report_format_kwargs,
-    ):
-        """
-
-        :param name: Name of the report
-        :param report: Dictionary containing the report files
-        :param report_format: Function that converts the report dictionary to a string
-        :param report_format_kwargs: Addtional kwargs for the report format function
-        :param compress: Whether to compress the report or not
-        """
-        self.name = name
-        self.report = report
-        self.report_format = report_format
-        self.report_format_kwargs = report_format_kwargs
-        self.compress = compress
-
-        self.sis_command_line = sys.argv
-        self.mail_address = getattr(gs, "MAIL_ADDRESS", None)
-        self.config_path = tk.config_manager.current_config
-        assert self.config_path is not None, "Could not find config path"
-
-        for i in ["date", "user", "name", "config_path", "sis_command_line"]:
-            assert i not in report, "%s will be set automatically"
-
-        self.out_report = self.output_path("report.gz")
-
-    def run(self):
-        user = getpass.getuser()
-        report = self.report.copy()
-
-        report["date"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        report["user"] = user
-        report["name"] = self.name
-        report["config_path"] = self.config_path
-        report["sis_command_line"] = str(self.sis_command_line)
-
-        if self.compress:
-            with gzip.open(self.out_report.get_path(), "w") as f:
-                f.write(
-                    self.report_format(report, **self.report_format_kwargs).encode()
-                    + b"\n"
-                )
+        if self.send_contents:
+            value = self.sh(
+                f"zcat -f {self.result.get_path()} | mail -s '{subject}' {self.mail_address}"
+            )
         else:
-            with open(self.out_report.get_path(), "w") as f:
-                f.write(self.report_format(report, **self.report_format_kwargs) + "\n")
-
-        if self.mail_address:
-            self.sh(
-                "zcat -f {out_report} | mail -s 'Report finished: {name} {config_path}' {mail_address}"
+            value = self.sh(
+                f"echo '{subject}' | mail -s '{subject}' {self.mail_address}"
             )
 
-    def tasks(self):
-        yield Task("run", mini_task=True)
+        self.out_status.set(value)
 
 
 def gmm_example_report_format(report: _Report_Type) -> str:
