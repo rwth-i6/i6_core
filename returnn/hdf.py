bb9a21ab1b5d57299e4c51ab34cfd8880c7e4449
Nick Rossenbach <rossenbach@i6.informatik.rwth-aachen.de>
2023-04-12 14:14:55 +0200

    remove cached=False

--- returnn/hdf.py
+++ returnn/hdf.py
@@ -309,7 +309,7 @@ class RasrAlignmentDumpHDFJob(Job):
         self.alignment_caches = alignment_caches
         self.allophone_file = allophone_file
         self.state_tying_file = state_tying_file
-        self.out_hdf_files = [self.output_path(f"data.hdf.{d}", cached=False) for d in range(len(alignment_caches))]
+        self.out_hdf_files = [self.output_path(f"data.hdf.{d}") for d in range(len(alignment_caches))]
         self.out_excluded_segments = self.output_path(f"excluded.segments", cached=False)
         self.returnn_root = returnn_root
         self.data_type = data_type
