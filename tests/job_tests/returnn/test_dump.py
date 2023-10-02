986589bcced7239c33b17fa92489aa77fae8fd13
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2023-10-02 09:55:41 +0200

    fix python

--- tests/job_tests/returnn/test_dump.py
+++ tests/job_tests/returnn/test_dump.py
@@ -19,18 +19,14 @@ def test_hdf_dump():
             f.write("#!rnn.py\n")
             f.write("train = {'class': 'DummyDataset', 'input_dim': 3, 'output_dim': 4, 'num_seqs': 2}")
         data = rel_path(f"{tmpdir}/tmp.config")
-        job = ReturnnDumpHDFJob(
-            data=data, returnn_root=tk.Path("returnn/"), returnn_python_exe=tk.Path("/usr/bin/python3")
-        )
+        job = ReturnnDumpHDFJob(data=data, returnn_root=tk.Path("returnn/"))
         assert [task.name() for task in job.tasks()] == ["run"]
         job._sis_setup_directory()
         job.run()
 
         # Case 2: dict
         data2 = {"class": "DummyDataset", "input_dim": 3, "output_dim": 4, "num_seqs": 2}
-        job2 = ReturnnDumpHDFJob(
-            data=data2, returnn_root=tk.Path("returnn/"), returnn_python_exe=tk.Path("/usr/bin/python3")
-        )
+        job2 = ReturnnDumpHDFJob(data=data2, returnn_root=tk.Path("returnn/"))
         assert [task.name() for task in job2.tasks()] == ["write_config", "run"]
         job2._sis_setup_directory()
         job2.write_config()
@@ -38,9 +34,7 @@ def test_hdf_dump():
 
         # Case 3: str
         data3 = "{'class': 'DummyDataset', 'input_dim': 3, 'output_dim': 4, 'num_seqs': 2}"
-        job3 = ReturnnDumpHDFJob(
-            data=data3, returnn_root=tk.Path("returnn/"), returnn_python_exe=tk.Path("/usr/bin/python3")
-        )
+        job3 = ReturnnDumpHDFJob(data=data3, returnn_root=tk.Path("returnn/"))
         assert [task.name() for task in job3.tasks()] == ["write_config", "run"]
         job3._sis_setup_directory()
         job3.write_config()
