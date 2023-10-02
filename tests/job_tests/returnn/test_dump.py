d8387d549579268b04f1167d098bf9f8b83e9589
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2023-10-02 09:44:02 +0200

    dump + test

--- tests/job_tests/returnn/test_dump.py
+++ tests/job_tests/returnn/test_dump.py
@@ -1,6 +1,5 @@
 import os
 import tempfile
-from typing import Optional
 
 from sisyphus import tk, setup_path
 
@@ -14,11 +13,41 @@ def test_hdf_dump():
         from sisyphus import gs
 
         gs.WORK_DIR = tmpdir
-        with open(f"{tmpdir}/tmp.config", 'wt') as f:
+
+        # Case 1: tk.Path
+        with open(f"{tmpdir}/tmp.config", "wt") as f:
             f.write("#!rnn.py\n")
             f.write("train = {'class': 'DummyDataset', 'input_dim': 3, 'output_dim': 4, 'num_seqs': 2}")
         data = rel_path(f"{tmpdir}/tmp.config")
-        job = ReturnnDumpHDFJob(data=data)
-        for task in ReturnnDumpHDFJob.tasks():
-            task()
-        
+        job = ReturnnDumpHDFJob(
+            data=data, returnn_root=tk.Path("returnn/"), returnn_python_exe=tk.Path("/usr/bin/python3")
+        )
+        assert [task.name() for task in job.tasks()] == ["run"]
+        job._sis_setup_directory()
+        job.run()
+
+        # Case 2: dict
+        data2 = {"class": "DummyDataset", "input_dim": 3, "output_dim": 4, "num_seqs": 2}
+        job2 = ReturnnDumpHDFJob(
+            data=data2, returnn_root=tk.Path("returnn/"), returnn_python_exe=tk.Path("/usr/bin/python3")
+        )
+        assert [task.name() for task in job2.tasks()] == ["write_config", "run"]
+        job2._sis_setup_directory()
+        job2.write_config()
+        job2.run()
+
+        # Case 3: str
+        data3 = "{'class': 'DummyDataset', 'input_dim': 3, 'output_dim': 4, 'num_seqs': 2}"
+        job3 = ReturnnDumpHDFJob(
+            data=data3, returnn_root=tk.Path("returnn/"), returnn_python_exe=tk.Path("/usr/bin/python3")
+        )
+        assert [task.name() for task in job3.tasks()] == ["write_config", "run"]
+        job3._sis_setup_directory()
+        job3.write_config()
+        job3.run()
+
+        assert os.path.getsize(job.out_hdf) == os.path.getsize(job2.out_hdf) == os.path.getsize(job3.out_hdf), (
+            os.path.getsize(job.out_hdf),
+            os.path.getsize(job2.out_hdf),
+            os.path.getsize(job3.out_hdf),
+        )
