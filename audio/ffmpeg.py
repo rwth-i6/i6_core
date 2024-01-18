ddceb75f051c1f4645c7ebc82b1f7a00a54ef6d1
Nick Rossenbach <rossenbach@cs.rwth-aachen.de>
2024-01-18 11:19:50 +0900

    Change BlissFfmpegJob threading
     - force ffmpeg to use 1 thread for input and output processing each
     - set higher number of default cpu cores
     - remote some tk.uncached_path calls

--- audio/ffmpeg.py
+++ audio/ffmpeg.py
@@ -112,7 +112,8 @@ class BlissFfmpegJob(Job):
         if self.error_threshold > 0:
             self.out_failed_files = self.output_path("failed_files.txt")
 
-        self.rqmt = {"time": 4, "cpu": 4, "mem": 8}
+        # e.g. 1 core for python and 4x2 cores for ffmpeg, one for input processing and one for output processing
+        self.rqmt = {"time": 4, "cpu": 9, "mem": 8}
 
     def tasks(self):
         yield Task("run", rqmt=self.rqmt)
@@ -124,11 +125,11 @@ class BlissFfmpegJob(Job):
 
     def run(self):
         c = corpus.Corpus()
-        c.load(tk.uncached_path(self.corpus_file))
+        c.load(self.corpus_file.get_path())
 
         from multiprocessing import pool
 
-        p = pool.Pool(self.rqmt["cpu"])
+        p = pool.Pool(self.rqmt["cpu"] // 2)
         p.map(self._perform_ffmpeg, c.all_recordings())
 
         for r in c.all_recordings():
@@ -138,7 +139,7 @@ class BlissFfmpegJob(Job):
         if self.recover_duration:
             c.dump("temp_corpus.xml.gz")
         else:
-            c.dump(tk.uncached_path(self.out_corpus))
+            c.dump(self.out_corpus.get_path())
 
         if self.out_failed_files is not None:
             with open(self.out_failed_files.get_path(), "wt") as out:
@@ -195,13 +196,9 @@ class BlissFfmpegJob(Job):
         target = os.path.join(self.out_audio_folder.get_path(), audio_filename)
         if not os.path.exists(target):
             logging.info(f"try converting {target}")
-            command_head = [
-                self.ffmpeg_binary,
-                "-hide_banner",
-                "-y",
-            ]
+            command_head = [self.ffmpeg_binary, "-hide_banner", "-y", "-threads", "1"]
             command_in = ["-i", recording.audio]
-            command_out = [target]
+            command_out = ["-threads", "1", target]
             in_options = self.ffmpeg_input_options or []
             out_options = self.ffmpeg_options or []
             command = command_head + in_options + command_in + out_options + command_out
