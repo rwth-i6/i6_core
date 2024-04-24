e3e12ffc3105eda68521f1708814c55ec0f22349
Albert Zeyer <zeyer@cs.rwth-aachen.de>
2024-04-24 12:10:23 +0200

    SearchOutputRawReplaceJob (#499)
    Co-authored-by: michelwi <michelwi@users.noreply.github.com>

--- returnn/search.py
+++ returnn/search.py
@@ -15,7 +15,7 @@ import logging
 import os
 import shutil
 import subprocess as sp
-from typing import Any, Union, Set, Dict
+from typing import Any, Union, Sequence, Set, Dict, Tuple
 
 from sisyphus import *
 
@@ -363,6 +363,60 @@ class SearchBPEtoWordsJob(Job):
                     out.write("],\n")
                 else:
                     out.write("%r: %r,\n" % (seq_tag, entry.replace("@@ ", "")))
+            out.write("}\n")
+
+
+class SearchOutputRawReplaceJob(Job):
+    """
+    Converts via replacement list.
+
+    Generalizes over :class:`SearchBPEtoWordsJob`.
+    BPE-to-words::
+
+        words = SearchOutputRawReplaceJob(bpe, [("@@ ", "")], output_gzip=True).out_search_results
+
+    SentencePiece-to-words::
+
+        words = SearchOutputRawReplaceJob(spm, [(" ", ""), ("▁", " ")], output_gzip=True).out_search_results
+
+    """
+
+    def __init__(
+        self, search_py_output: Path, replacement_list: Sequence[Tuple[str, str]], *, output_gzip: bool = False
+    ):
+        """
+        :param search_py_output: a search output file from RETURNN in python format (single or n-best)
+        :param replacement_list: list/sequence of (old, new) pairs to perform ``s.replace(old,new)`` on the raw text
+        :param output_gzip: if True, gzip the output
+        """
+        self.search_py_output = search_py_output
+        self.replacement_list = replacement_list
+        self.out_search_results = self.output_path("search_results.py" + (".gz" if output_gzip else ""))
+
+    def tasks(self):
+        yield Task("run", mini_task=True)
+
+    def run(self):
+        d = eval(util.uopen(self.search_py_output, "rt").read(), {"nan": float("nan"), "inf": float("inf")})
+        assert isinstance(d, dict)  # seq_tag -> bpe string
+        assert not os.path.exists(self.out_search_results.get_path())
+
+        def _transform_text(s: str):
+            for in_, out_ in self.replacement_list:
+                s = s.replace(in_, out_)
+            return s
+
+        with util.uopen(self.out_search_results, "wt") as out:
+            out.write("{\n")
+            for seq_tag, entry in d.items():
+                if isinstance(entry, list):
+                    # n-best list as [(score, text), ...]
+                    out.write("%r: [\n" % (seq_tag,))
+                    for score, text in entry:
+                        out.write("(%f, %r),\n" % (score, _transform_text(text)))
+                    out.write("],\n")
+                else:
+                    out.write("%r: %r,\n" % (seq_tag, _transform_text(entry)))
             out.write("}\n")
 
 
