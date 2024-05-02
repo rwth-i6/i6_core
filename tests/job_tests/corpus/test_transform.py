a1b096764bc0eb003d77ba1bb42df0f0503fdd60
Nahuel Unai Roselló Beneitez <nbeneitez@apptek.com>
2024-05-02 11:34:08 +0000

    Try to make pytest happy by skipping useless tests

--- tests/job_tests/corpus/test_transform.py
+++ tests/job_tests/corpus/test_transform.py
@@ -10,89 +10,90 @@ import i6_core.lib.corpus as libcorpus
 Path = setup_path(__package__)
 
 
-@pytest.mark.skip(reason="Private method only used in file.")
-def _create_corpus_with_structure(corpus_dict: dict[str, Any]) -> libcorpus.Corpus:
-    """
-    Creates a simple corpus from its definition in dictionary form.
-    The dictionary can have the following keys and with the respective type:
-    - "name": str
-    - "recordings": list[dict[str, Any]]: can have the following keys:
+@pytest.mark.skip(reason="Helper class only used in file.")
+class CorpusCreatorHelper:
+    @staticmethod
+    def _create_corpus_with_structure(corpus_dict: dict[str, Any]) -> libcorpus.Corpus:
+        """
+        Creates a simple corpus from its definition in dictionary form.
+        The dictionary can have the following keys and with the respective type:
         - "name": str
-        - "segments": list[str]
-    - "subcorpora": list[dict[str, Any]]: recursive definition of a corpus.
+        - "recordings": list[dict[str, Any]]: can have the following keys:
+            - "name": str
+            - "segments": list[str]
+        - "subcorpora": list[dict[str, Any]]: recursive definition of a corpus.
 
-    :param corpus_dict: Definition of a corpus in dictionary form.
-    :return: Corpus object defined by the corpus dictionary provided.
-    """
-    corpus = libcorpus.Corpus()
-    corpus.name = corpus_dict["name"]
-    for recording_dict in corpus_dict.get("recordings", []):
-        recording = libcorpus.Recording()
-        recording.name = recording_dict["name"]
-        for segment_name in recording_dict.get("segments", []):
-            segment = libcorpus.Segment()
-            segment.name = segment_name
-            segment.orth = ""
-            recording.add_segment(segment)
-        corpus.add_recording(recording)
-    for subcorpus_dict in corpus_dict.get("subcorpora", []):
-        corpus.add_subcorpus(_create_corpus_with_structure(subcorpus_dict))
+        :param corpus_dict: Definition of a corpus in dictionary form.
+        :return: Corpus object defined by the corpus dictionary provided.
+        """
+        corpus = libcorpus.Corpus()
+        corpus.name = corpus_dict["name"]
+        for recording_dict in corpus_dict.get("recordings", []):
+            recording = libcorpus.Recording()
+            recording.name = recording_dict["name"]
+            for segment_name in recording_dict.get("segments", []):
+                segment = libcorpus.Segment()
+                segment.name = segment_name
+                segment.orth = ""
+                recording.add_segment(segment)
+            corpus.add_recording(recording)
+        for subcorpus_dict in corpus_dict.get("subcorpora", []):
+            corpus.add_subcorpus(CorpusCreatorHelper._create_corpus_with_structure(subcorpus_dict))
 
-    return corpus
+        return corpus
 
+    @staticmethod
+    def _check_corpus_with_structure(corpus: libcorpus.Corpus, corpus_dict: dict[str, Any]) -> bool:
+        """
+        Asserts that the corpus passed as parameter has the same structure as the simple corpus dictionary provided.
 
-@pytest.mark.skip(reason="Private method only used in file.")
-def _check_corpus_with_structure(corpus: libcorpus.Corpus, corpus_dict: dict[str, Any]) -> bool:
-    """
-    Asserts that the corpus passed as parameter has the same structure as the simple corpus dictionary provided.
-
-    :param corpus: Corpus for which to check the structure.
-    :param corpus_dict: Expected corpus structure.
-    :return: True if the corpus matches the given structure.
-        Doesn't return False whenever the corpus structure doesn't match, but fails with an assertion.
-    """
-    # Check that the corpus name matches.
-    assert (
-        corpus.name == corpus_dict["name"]
-    ), f"Corpus name ({corpus.name}) doesn't coincide with provided ({corpus_dict['name']})."
-
-    # Check that the recordings (and their internal segments) match.
-    recordings_dicts = corpus_dict.get("recordings", [])
-    assert len(corpus.recordings) == len(recordings_dicts), (
-        f"Number of recordings in corpus ({len(corpus.recordings)}) not coinciding "
-        f"with provided ({len(recordings_dicts)})."
-    )
-    for recording_dict in recordings_dicts:
-        segments_from_dict = recording_dict.get("segments", [])
-
-        # Get the recording with the same name.
-        recordings_in_corpus_with_same_name = [
-            r for r in corpus.top_level_recordings() if r.name == recording_dict["name"]
-        ]
-        found_recording = None
-        for r in recordings_in_corpus_with_same_name:
-            if sorted([segment.name for segment in r.segments]) == sorted(segments_from_dict):
-                found_recording = r
-                break
+        :param corpus: Corpus for which to check the structure.
+        :param corpus_dict: Expected corpus structure.
+        :return: True if the corpus matches the given structure.
+            Doesn't return False whenever the corpus structure doesn't match, but fails with an assertion.
+        """
+        # Check that the corpus name matches.
         assert (
-            found_recording
-        ), f"No recording in {corpus.fullname()} found with the features provided by the user: {recording_dict}. "
+            corpus.name == corpus_dict["name"]
+        ), f"Corpus name ({corpus.name}) doesn't coincide with provided ({corpus_dict['name']})."
 
-    # Recursively check that the subcorpora match.
-    subcorpus_dicts = corpus_dict.get("subcorpora", [])
-    for subcorpus_dict in subcorpus_dicts:
-        matching_subcorpus = [sc for sc in corpus.top_level_subcorpora() if sc.name == subcorpus_dict["name"]]
-        assert (
-            len(matching_subcorpus) == 1
-        ), f"There's more than one subcorpus matching for the subcorpus name {subcorpus_dict['name']}"
-        _check_corpus_with_structure(matching_subcorpus[0], subcorpus_dict)
+        # Check that the recordings (and their internal segments) match.
+        recordings_dicts = corpus_dict.get("recordings", [])
+        assert len(corpus.recordings) == len(recordings_dicts), (
+            f"Number of recordings in corpus ({len(corpus.recordings)}) not coinciding "
+            f"with provided ({len(recordings_dicts)})."
+        )
+        for recording_dict in recordings_dicts:
+            segments_from_dict = recording_dict.get("segments", [])
 
-    return True
+            # Get the recording with the same name.
+            recordings_in_corpus_with_same_name = [
+                r for r in corpus.top_level_recordings() if r.name == recording_dict["name"]
+            ]
+            found_recording = None
+            for r in recordings_in_corpus_with_same_name:
+                if sorted([segment.name for segment in r.segments]) == sorted(segments_from_dict):
+                    found_recording = r
+                    break
+            assert (
+                found_recording
+            ), f"No recording in {corpus.fullname()} found with the features provided by the user: {recording_dict}. "
+
+        # Recursively check that the subcorpora match.
+        subcorpus_dicts = corpus_dict.get("subcorpora", [])
+        for subcorpus_dict in subcorpus_dicts:
+            matching_subcorpus = [sc for sc in corpus.top_level_subcorpora() if sc.name == subcorpus_dict["name"]]
+            assert (
+                len(matching_subcorpus) == 1
+            ), f"There's more than one subcorpus matching for the subcorpus name {subcorpus_dict['name']}"
+            CorpusCreatorHelper._check_corpus_with_structure(matching_subcorpus[0], subcorpus_dict)
+
+        return True
 
 
-def test_merge_corpora_job_merge_subcorpus_and_recordings():
+def test_merge_corpora_job_merge_recursive():
     """
-    Merges two hardcoded corpora with the `MergeStrategy.MERGE_SUBCORPORA` whose structures are the following:
+    Merges two hardcoded corpora with the `MergeStrategy.MERGE_RECURSIVE` whose structures are the following:
 
     CORPUS 1            CORPUS 2
     rec1                rec1
@@ -142,10 +143,10 @@ def test_merge_corpora_job_merge_subcorpus_and_recordings():
         rec2
 
     Some notes about the expected output:
-    `MergeStrategy.MERGE_SUBCORPORA` only extends the segment list of the recordings.
+    `MergeStrategy.MERGE_RECURSIVE` only extends the segment list of the recordings.
     - The nested subcorpus sc2/sc1 shouldn't be merged with the main subcorpus sc1.
     Moreover, its recording sc2/sc1/rec1 shouldn't be merged with neither rec1 nor sc1/rec1.
-    `MergeStrategy.MERGE_SUBCORPORA` only takes into consideration the top level subcorpora.
+    `MergeStrategy.MERGE_RECURSIVE` only takes into consideration the top level subcorpora.
     - It's intended that sc3/rec2 doesn't have any segments.
     """
     with tempfile.TemporaryDirectory() as tmpdir:
@@ -173,7 +174,7 @@ def test_merge_corpora_job_merge_subcorpus_and_recordings():
                 },
             ],
         }
-        c1 = _create_corpus_with_structure(c1_structure)
+        c1 = CorpusCreatorHelper._create_corpus_with_structure(c1_structure)
         c1.dump(c1_path)
 
         # Second corpus.
@@ -203,7 +204,7 @@ def test_merge_corpora_job_merge_subcorpus_and_recordings():
                 },
             ],
         }
-        c2 = _create_corpus_with_structure(c2_structure)
+        c2 = CorpusCreatorHelper._create_corpus_with_structure(c2_structure)
         c2.dump(c2_path)
 
         # Merge the first and second corpora.
@@ -211,7 +212,7 @@ def test_merge_corpora_job_merge_subcorpus_and_recordings():
         merge_corpora_job = MergeCorporaJob(
             bliss_corpora=[Path(c1_path), Path(c2_path)],
             name="custom_name_for_merged_corpus",
-            merge_strategy=MergeStrategy.MERGE_SUBCORPORA,
+            merge_strategy=MergeStrategy.MERGE_RECURSIVE,
         )
         merge_corpora_job.out_merged_corpus = Path(merged_corpus_path)
         merge_corpora_job.run()
@@ -253,4 +254,4 @@ def test_merge_corpora_job_merge_subcorpus_and_recordings():
                 },
             ],
         }
-        _check_corpus_with_structure(merged_corpus, expected_merged_corpus_structure)
+        CorpusCreatorHelper._check_corpus_with_structure(merged_corpus, expected_merged_corpus_structure)
