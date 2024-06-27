5d4473528236dd5d9494a29380605f3ad4c216b3
Javier Jorge <JJorgeDSIC@users.noreply.github.com>
2024-06-27 14:57:39 +0200

    Update ComputePerplexityJob with additional optional outputs (#520)
    Co-authored-by: Javier Jorge Cano <jjorge@apptek.com>

--- lm/perplexity.py
+++ lm/perplexity.py
@@ -33,7 +33,13 @@ class ComputePerplexityJob(rasr.RasrCommand, Job):
 
         self.log_file = self.log_file_output_path("compute_ppl", crp, False)
         self.score_file = self.output_path("word.scores")
+        self.num_tokens = self.output_var("num-tokens")
+        self.num_unks = self.output_var("num-unks")
+        self.unk_ratio = self.output_var("unk-ratio")
         self.perplexity = self.output_var("perplexity")
+        self.perplexity_without_eos = self.output_var("perplexity-without-eos")
+        self.perplexity_without_unknowns = self.output_var("perplexity-without-unknowns")
+        self.perplexity_without_eos_without_unknowns = self.output_var("perplexity-without-eos-without-unknowns")
 
         self.rqmt = {"time": 1, "cpu": 1, "mem": 2}
 
@@ -59,6 +65,43 @@ class ComputePerplexityJob(rasr.RasrCommand, Job):
             root = ET.parse(f)
         ppl = root.find(".//perplexity")
         self.perplexity.set(float(ppl.text))
+
+        # Retrocompatibility checks
+        ppl_wo_eos = root.find(".//perplexity-without-eos")
+        if ppl_wo_eos is not None:
+            self.perplexity_without_eos.set(float(ppl_wo_eos.text))
+        else:
+            self.perplexity_without_eos.set(float("inf"))
+
+        ppl_wo_unks = root.find(".//perplexity-without-unknowns")
+        if ppl_wo_unks is not None:
+            self.perplexity_without_unknowns.set(float(ppl_wo_unks.text))
+        else:
+            self.perplexity_without_unknowns.set(float("inf"))
+
+        ppl_wo_eos_wo_unks = root.find(".//perplexity-without-eos-without-unknowns")
+        if ppl_wo_eos_wo_unks is not None:
+            self.perplexity_without_eos_without_unknowns.set(float(ppl_wo_eos_wo_unks.text))
+        else:
+            self.perplexity_without_eos_without_unknowns.set(float("inf"))
+
+        num_tokens = root.find(".//num-tokens")
+        if num_tokens is not None:
+            self.num_tokens.set(float(num_tokens.text))
+        else:
+            self.num_tokens.set(float("inf"))
+
+        num_unks = root.find(".//num-unks")
+        if num_unks is not None:
+            self.num_unks.set(float(num_unks.text))
+        else:
+            self.num_unks.set(float("inf"))
+
+        ratio_unks = root.find(".//unk-ratio")
+        if ratio_unks is not None:
+            self.unk_ratio.set(float(ratio_unks.text))
+        else:
+            self.unk_ratio.set(float("inf"))
 
     def cleanup_before_run(self, cmd, retry, *args):
         util.backup_if_exists("compute_ppl.log")
