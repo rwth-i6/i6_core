7be4cbf75eae1c3ab1405fb7877b1b3d07f02a07
Nahuel Unai Roselló Beneitez <31628502+Icemole@users.noreply.github.com>
2024-09-04 09:42:44 +0200

    Black v2

--- mm/alignment.py
+++ mm/alignment.py
@@ -3,7 +3,7 @@ __all__ = [
     "DumpAlignmentJob",
     "PlotAlignmentJob",
     "AMScoresFromAlignmentLogJob",
-    "ComputeTimeStampErrorJob"
+    "ComputeTimeStampErrorJob",
 ]
 
 import logging
