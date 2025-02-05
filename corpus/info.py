f7169084b24dae23e1b61f088bffb78ed6d7d1af
Nahuel Unai Roselló Beneitez <nbeneitez@apptek.com>
2025-02-05 03:14:42 -0500

    Add duration file delimiter

--- corpus/info.py
+++ corpus/info.py
@@ -11,14 +11,18 @@ class DumpRecordingPathsJob(Job):
     Dump all recordings of a given corpus file, one audio per line.
     """
 
-    def __init__(self, corpus_file: tk.Path, dump_durations: bool = False):
+    def __init__(self, corpus_file: tk.Path, dump_durations: bool = False, duration_file_delimiter: str = "\t"):
         r"""
         :param corpus_file: Corpus file from which to obtain the audio list.
         :param dump_durations: Whether to dump the durations of the audios along with the audio list.
-            The `out_audio_durations` output file contains `<audio>\t<duration>` per audio/duration pair.
+            The `out_audio_durations` output file will contain an audio/duration pair,
+            separated by :param:`duration_file_delimiter`.
+        :param duration_file_delimiter: Delimiter of the duration file. Defaults to `\t`.
+            It only makes sense to override this whenever :param:`dump_durations` is `True`.
         """
         self.corpus_file = corpus_file
         self.dump_durations = dump_durations
+        self.duration_file_delimiter = duration_file_delimiter
 
         self.out_audio_list = self.output_path("out.txt")
         if dump_durations:
@@ -41,4 +45,4 @@ class DumpRecordingPathsJob(Job):
             with open(self.out_audio_durations.get_path(), "w") as f:
                 for r in c.all_recordings():
                     duration = compute_rec_duration(r.audio)
-                    f.write(f"{r.audio}\t{duration}")
+                    f.write(f"{r.audio}{self.duration_file_delimiter}{duration}")
