de28f0a5b1c8664a9c008389027ef8a69441d15c
Nahuel Unai Roselló Beneitez <nbeneitez@apptek.com>
2025-02-05 03:09:15 -0500

    Reuse exception

--- lib/audio.py
+++ lib/audio.py
@@ -22,11 +22,12 @@ def compute_rec_duration(rec_audio: str) -> float:
     if rec_audio.endswith("mp3"):
         try:
             audio_length = MP3(rec_audio).info.length
-        except ImportError:
-            raise ImportError(
+        except ImportError as ie:
+            ie.msg = (
                 "The 'mutagen' module is required to calculate the duration of the mp3 file "
                 f"{rec_audio}, but it's not installed in your system."
             )
+            raise ie
         return audio_length
     elif rec_audio.endswith("aac"):
         # The aac format has unreliable timestamps. Convert to wav on the fly for an accurate measurement.
