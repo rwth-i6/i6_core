cf4ace28c221803e3a30475b73fee79ba5df6320
Nahuel Unai Roselló Beneitez <nbeneitez@apptek.com>
2025-02-05 03:04:07 -0500

    Add function to compute the duration of a recording

---
+++ lib/audio.py
@@ -0,0 +1,39 @@
+__all__ = ["compute_rec_duration"]
+
+
+import subprocess as sp
+import tempfile
+
+try:
+    from mutagen.mp3 import MP3
+except ImportError:
+    # No mutagen module. MP3 conversion will not be supported.
+    pass
+import soundfile as sf
+
+
+def compute_rec_duration(rec_audio: str) -> float:
+    """
+    Computes the duration of a given recording in seconds.
+
+    :param str rec_audio: Path of the recording.
+        The accepted formats are MP3, WAV, AAC or any audio format parseable by soundfile.
+    """
+    if rec_audio.endswith("mp3"):
+        try:
+            audio_length = MP3(rec_audio).info.length
+        except ImportError:
+            raise ImportError("mutagen module not installed.")
+        return audio_length
+    elif rec_audio.endswith("aac"):
+        # The aac format has unreliable timestamps. Convert to wav on the fly for an accurate measurement.
+        with tempfile.NamedTemporaryFile(suffix=".wav") as tmp_audio:  # Reserve a temporary file.
+            # Convert to wav.
+            sp.run(
+                ["ffmpeg", "-loglevel", "quiet", "-i", rec_audio, "-y", tmp_audio.name]  # Overwrite the temporary file.
+            )
+            return compute_rec_duration(tmp_audio.name)  # Now in wav format.
+    else:
+        # Wav or any format parseable by soundfile.
+        with sf.SoundFile(rec_audio) as f:
+            return f.frames / f.samplerate  # In seconds.
