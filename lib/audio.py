378ab4c6a48fee022bf25d49bf12ecad68b0181c
Nahuel Unai Roselló Beneitez <nbeneitez@apptek.com>
2025-02-10 04:18:33 -0500

    rec_audio -> audio_file for clarity
    The functions here have no context about "rec" (recording)

--- lib/audio.py
+++ lib/audio.py
@@ -8,12 +8,12 @@ import tempfile
 import soundfile as sf
 
 
-def _compute_rec_duration_convert_to_wav(rec_audio: str) -> float:
+def _compute_rec_duration_convert_to_wav(audio_file: str) -> float:
     """
     Tries to compute the recording duration by converting the file provided as parameter to wav.
     Needs ffmpeg to be installed in the system. Raises `FileNotFoundError` if not present.
 
-    :return: Duration of :param:`rec_audio` converted to wav.
+    :return: Duration of :param:`audio_file` converted to wav.
     """
     with tempfile.NamedTemporaryFile(suffix=".wav") as tmp_audio:  # Reserve a temporary file.
         # Convert to wav.
@@ -23,7 +23,7 @@ def _compute_rec_duration_convert_to_wav(rec_audio: str) -> float:
                 "-loglevel",
                 "quiet",
                 "-i",
-                rec_audio,
+                audio_file,
                 "-y",
                 tmp_audio.name,
             ]  # Overwrite the temporary file.
@@ -31,44 +31,44 @@ def _compute_rec_duration_convert_to_wav(rec_audio: str) -> float:
         return compute_rec_duration(tmp_audio.name)  # Now in wav format.
 
 
-def compute_rec_duration(rec_audio: str) -> float:
+def compute_rec_duration(audio_file: str) -> float:
     """
     Computes the duration of a given recording in seconds.
 
-    :param str rec_audio: Path of the recording.
+    :param str audio_file: Path of the recording.
         The accepted formats are mp3, wav, aac or any audio format parseable by soundfile.
     """
-    if rec_audio.endswith("mp3"):
+    if audio_file.endswith("mp3"):
         try:
             from mutagen.mp3 import MP3
 
-            return MP3(rec_audio).info.length
+            return MP3(audio_file).info.length
         except ImportError:
             logging.warning(
                 "The 'mutagen' module doesn't exist. We recommend installing it. "
                 "Falling back to wav conversion (much slower)..."
             )
             try:
-                return _compute_rec_duration_convert_to_wav(rec_audio)
+                return _compute_rec_duration_convert_to_wav(audio_file)
             except FileNotFoundError:
                 # ffmpeg doesn't exist either.
                 raise FileNotFoundError(
                     f"mutagen python module not found, required to calculate duration of mp3 file.\n"
                     "ffmpeg binary not found either, so mp3 -> wav conversion can't succeed.\n"
                     "Please either install the mutagen python module or the ffmpeg binary.\n"
-                    f"Can't read duration of mp3 file: {rec_audio}."
+                    f"Can't read duration of mp3 file: {audio_file}."
                 )
-    elif rec_audio.endswith("aac"):
+    elif audio_file.endswith("aac"):
         # The aac format has unreliable timestamps. Convert to wav through ffmpeg for an accurate measurement.
         try:
-            return _compute_rec_duration_convert_to_wav(rec_audio)
+            return _compute_rec_duration_convert_to_wav(audio_file)
         except FileNotFoundError:
             # ffmpeg doesn't exist.
             raise FileNotFoundError(
                 "ffmpeg binary not found, so aac -> wav conversion can't succeed.\n"
-                f"Refusing to give unreliable timestamp of aac file {rec_audio}."
+                f"Refusing to give unreliable timestamp of aac file {audio_file}."
             )
     else:
         # Wav or any format parseable by soundfile.
-        with sf.SoundFile(rec_audio) as f:
+        with sf.SoundFile(audio_file) as f:
             return f.frames / f.samplerate  # In seconds.
