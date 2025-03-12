8a2a9f06402ef741fc896073817a3662a47aa710
Nahuel Unai Roselló Beneitez <nbeneitez@apptek.com>
2025-03-12 10:53:26 -0400

    More verbose parameter, improve docstring

--- corpus/transform.py
+++ corpus/transform.py
@@ -640,27 +640,25 @@ class MapRecordingsJob(Job):
     Applies a function to all recordings of a given corpus.
     """
 
-    __sis_hash_exclude__ = {"reformat_orth": True}
+    __sis_hash_exclude__ = {"corpus_load_reformat_orth": True}
 
     def __init__(
         self,
         bliss_corpus: tk.Path,
         recording_callable: Callable[[corpus.Recording], corpus.Recording],
-        reformat_orth: bool = True,
+        corpus_load_reformat_orth: bool = True,
     ):
         """
         :param bliss_corpus: Corpus for which to sort the segments.
         :param recording_callable: Callable to modify a given recording. Returns the modified recording.
-        :param reformat_orth: Whether to do some processing of the text
-            that goes into the orth tag to get a nicer-looking formating.
-            If `True`, removes multiline content in the orth tag, leading/trailing spaces,
+        :param reformat_orth: If `True`, the corpus loading will remove newline characters
             and multiple spaces inside the text.
 
             Defaults to `True` (initial behavior of :class:`Corpus`).
         """
         self.bliss_corpus = bliss_corpus
         self.recording_callable = recording_callable
-        self.reformat_orth = reformat_orth
+        self.corpus_load_reformat_orth = corpus_load_reformat_orth
 
         self.out_bliss_corpus = self.output_path("out.xml.gz")
 
@@ -678,7 +676,7 @@ class MapRecordingsJob(Job):
 
     def run(self):
         c = corpus.Corpus()
-        c.load(self.bliss_corpus.get_path(), reformat_orth=self.reformat_orth)
+        c.load(self.bliss_corpus.get_path(), reformat_orth=self.corpus_load_reformat_orth)
 
         self.map_recordings(c, self.recording_callable)
         for sc in c.subcorpora:
