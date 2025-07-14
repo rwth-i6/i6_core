14247a5143888ca407d98d665cf7990e0428b53b
Nahuel Unai Roselló Beneitez <nbeneitez@apptek.com>
2025-07-14 06:37:12 -0400

    Fix

--- tests/job_tests/corpus/test_transform.py
+++ tests/job_tests/corpus/test_transform.py
@@ -30,8 +30,8 @@ class _CorpusCreatorHelper:
         corpus.name = corpus_dict["name"]
         for recording_dict in corpus_dict.get("recordings", []):
             recording = libcorpus.Recording()
-            corpus.add_recording(recording)
             recording.name = recording_dict["name"]
+            corpus.add_recording(recording)
             for segment_name in recording_dict.get("segments", []):
                 segment = libcorpus.Segment()
                 segment.name = segment_name
